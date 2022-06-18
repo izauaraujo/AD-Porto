3eccd2586e5a37ca37bf37c49db79465cd617e56
izauaraujo <izauaraujo@gmail.com>
2022-06-18 15:35:11 -0300

    alterado Requestmapping adportoUser

--- src/main/java/com/secretaria/ADPorto/controller/UserController.java
+++ src/main/java/com/secretaria/ADPorto/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.ExecutionException;
 
 @RestController
-@RequestMapping("/adporto")
+@RequestMapping("/adportoUser")
 
 public class UserController {
     @Autowired
