2524fc5bcca2ae7683821cf6c24a61a4bea29f8d
izauaraujo <izauaraujo@gmail.com>
2022-06-20 00:07:20 -0300

    alterado Requestmapping adportoUser

--- src/main/java/com/secretaria/ADPorto/service/UserService.java
+++ src/main/java/com/secretaria/ADPorto/service/UserService.java
@@ -3,15 +3,16 @@ package com.secretaria.ADPorto.service;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
+import com.secretaria.ADPorto.entity.User;
 
 public class UserService {
 
-    public static UserRecord createUser() throws FirebaseAuthException {
+    public static UserRecord createUser(User user) throws FirebaseAuthException {
         // [START create_user]
         UserRecord.CreateRequest request = new UserRecord.CreateRequest()
-                .setEmail("user@example.com")
+                .setEmail(user.getEmailUser())//"user@example.com"
                 .setEmailVerified(false)
-                .setPassword("secretPassword")
+                .setPassword(user.getPasswordUser())//"secretPassword"
                 .setPhoneNumber("+11234567890")
                 .setDisplayName("John Doe")
                 .setPhotoUrl("http://www.example.com/12345678/photo.png")
