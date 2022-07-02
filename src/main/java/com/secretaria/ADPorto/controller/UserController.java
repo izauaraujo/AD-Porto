8fedaaff7b062695e790a612c3b59f0b720c4320
izauaraujo <izauaraujo@gmail.com>
2022-07-02 18:16:21 -0300

    implementação de segurança 5

--- src/main/java/com/secretaria/ADPorto/controller/UserController.java
+++ src/main/java/com/secretaria/ADPorto/controller/UserController.java
@@ -7,9 +7,11 @@ import com.secretaria.ADPorto.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.ExecutionException;
 
+@RestController
 public class UserController {
 
     private UserService userService;
