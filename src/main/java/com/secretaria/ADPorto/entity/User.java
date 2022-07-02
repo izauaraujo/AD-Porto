466c405fe221067f76075290e189657cd5b63d84
izauaraujo <izauaraujo@gmail.com>
2022-07-01 22:31:43 -0300

    implementação de segurança 3

--- src/main/java/com/secretaria/ADPorto/entity/User.java
+++ src/main/java/com/secretaria/ADPorto/entity/User.java
@@ -1,16 +1,25 @@
 package com.secretaria.ADPorto.entity;
 
-public class User {
+public class   User {
 
-    private String passwordUser= "tatu ";
-    private String emailUser ="tatu@example.com";
+    private String nameUser;
+    private String passwordUser;
+    private String roleUser;
+    private String EmailUser;
 
+    public String getNameUser() { return nameUser; }
+
+    public void setNameUser(String nameUser) { this.nameUser = nameUser; }
 
     public String getPasswordUser() { return passwordUser; }
 
-    public void setPasswordUser(String passwordUser) {this.passwordUser = passwordUser; }
+    public void setPasswordUser(String passwordUser) { this.passwordUser = passwordUser;}
 
-    public String getEmailUser() { return emailUser; }
+    public String getRoleUser() { return roleUser; }
 
-    public void setEmailUser(String emailUser) { this.emailUser = emailUser; }
+    public void setRoleUser(String roleUser) {this.roleUser = roleUser; }
+
+    public String getEmailUser() {return EmailUser;}
+
+    public void setEmailUser(String emailUser) {EmailUser = emailUser;}
 }
