4ca2cad7072020c12e79a9efb7fba95bb2912304
izauaraujo <izauaraujo@gmail.com>
2022-07-09 15:29:06 -0300

    implementação de segurança 7

--- src/main/java/com/secretaria/ADPorto/controller/CongregationController.java
+++ src/main/java/com/secretaria/ADPorto/controller/CongregationController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @RestController
-@RequestMapping("/adporto")
+//@RequestMapping("/adporto")
 
 public class CongregationController {
 
