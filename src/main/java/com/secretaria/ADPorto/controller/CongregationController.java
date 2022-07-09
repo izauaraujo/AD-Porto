ae2b9c6edfd9031868a862945a8b63d706c6e811
izauaraujo <izauaraujo@gmail.com>
2022-07-09 13:58:47 -0300

    implementação de segurança 7

--- src/main/java/com/secretaria/ADPorto/controller/CongregationController.java
+++ src/main/java/com/secretaria/ADPorto/controller/CongregationController.java
@@ -1,17 +1,10 @@
 package com.secretaria.ADPorto.controller;
 
 
-import com.google.cloud.firestore.QueryDocumentSnapshot;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.UserRecord;
 import com.secretaria.ADPorto.entity.Member;
-import com.secretaria.ADPorto.entity.User;
 import com.secretaria.ADPorto.service.CongregationService;
-import com.secretaria.ADPorto.service.UserService;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
