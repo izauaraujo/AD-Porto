f4fa5de8433fb41989efa4f400fb9e4e6463105e
izauaraujo <izauaraujo@gmail.com>
2022-07-10 12:18:28 -0300

    implementação de segurança 7

--- src/main/java/com/secretaria/ADPorto/firebase/FirebaseInitialization.java
+++ src/main/java/com/secretaria/ADPorto/firebase/FirebaseInitialization.java
@@ -9,26 +9,21 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @Service
 public class FirebaseInitialization {
 
     @PostConstruct
-      public void initialization(){
+      public void initialization() throws IOException {
 
-          FileInputStream serviceAccount =
-                  null;
-          try {
-              serviceAccount = new FileInputStream("./serviceAccountKey.json");
+        FileInputStream serviceAccount =
+                new FileInputStream("path/to/serviceAccountKey.json");
 
-          FirebaseOptions options = new FirebaseOptions.Builder()
-                  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                  .build();
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
 
-          FirebaseApp.initializeApp(options);
-
-          } catch (Exception e) {
-              e.printStackTrace();
-          }
+        FirebaseApp.initializeApp(options);
     }
 }
