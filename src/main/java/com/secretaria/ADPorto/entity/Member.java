95b3c30b6a7244b14842d8ff978823b1e0956bbb
izauaraujo <izauaraujo@gmail.com>
2022-08-22 19:24:42 -0300

    implementação de segurança

--- src/main/java/com/secretaria/ADPorto/entity/Member.java
+++ src/main/java/com/secretaria/ADPorto/entity/Member.java
@@ -5,22 +5,44 @@ import java.util.Date;
 import java.util.Map;
 
 public class  Member {
-    private boolean active;
-   // private Integer registrationNumber;
+    private String situation;
+    private Integer memberRegistrationNumber;
     private String congregationName;
     private String memberName;
     private String fatherMemberName;
     private String motherMemberName;
-    // private Date dateBirth;  private String nationality;
+    private Date dateBirth;
+    private String nationality;
+    private String cityBirth;
+    private String stateBirth;
+    private String maritalStatus;
+    private String DateBaptism;
+    private String churchBaptism;
+    private String cityBaptism;
+    private String admissionDate;
+    private String CPF;
+    private String RG;
+    private String spouse;
+    private  String origination;
+    private String streetAvenue;
+    private int number;
+    private String note;
 
 
-//izau
-    public boolean isActive() {
-        return active;
+    public String getSituation() {
+        return situation;
     }
 
-    public void setActive(boolean active) {
-        this.active = active;
+    public void setSituation(String situation) {
+        this.situation = situation;
+    }
+
+    public Integer getMemberRegistrationNumber() {
+        return memberRegistrationNumber;
+    }
+
+    public void setMemberRegistrationNumber(Integer memberRegistrationNumber) {
+        this.memberRegistrationNumber = memberRegistrationNumber;
     }
 
     public String getCongregationName() {
@@ -55,5 +77,132 @@ public class  Member {
         this.motherMemberName = motherMemberName;
     }
 
+    public Date getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(Date dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getCityBirth() {
+        return cityBirth;
+    }
+
+    public void setCityBirth(String cityBirth) {
+        this.cityBirth = cityBirth;
+    }
+
+    public String getStateBirth() {
+        return stateBirth;
+    }
+
+    public void setStateBirth(String stateBirth) {
+        this.stateBirth = stateBirth;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    public String getDateBaptism() {
+        return DateBaptism;
+    }
+
+    public void setDateBaptism(String dateBaptism) {
+        DateBaptism = dateBaptism;
+    }
+
+    public String getChurchBaptism() {
+        return churchBaptism;
+    }
+
+    public void setChurchBaptism(String churchBaptism) {
+        this.churchBaptism = churchBaptism;
+    }
+
+    public String getCityBaptism() {
+        return cityBaptism;
+    }
+
+    public void setCityBaptism(String cityBaptism) {
+        this.cityBaptism = cityBaptism;
+    }
+
+    public String getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(String admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public String getSpouse() {
+        return spouse;
+    }
+
+    public void setSpouse(String spouse) {
+        this.spouse = spouse;
+    }
+
+    public String getOrigination() {
+        return origination;
+    }
+
+    public void setOrigination(String origination) {
+        this.origination = origination;
+    }
+
+    public String getStreetAvenue() {
+        return streetAvenue;
+    }
+
+    public void setStreetAvenue(String streetAvenue) {
+        this.streetAvenue = streetAvenue;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 }
 
---
+++ src/main/java/com/secretaria/ADPorto/entity/Obreiros.java
@@ -0,0 +1,41 @@
+package com.secretaria.ADPorto.entity;
+
+import java.util.Date;
+
+public class Obreiros {
+
+    private String situation;
+    private String memberRegistrationNumber;
+    private String Congregation;
+    private String WorkerRegistrationNumber;
+    private String position;
+    private String name;
+    private Date dateBirth;
+    private String street;
+    private String number;
+    private String district;
+    private String city;
+    private String RG;
+    private String CPF;
+    private String maritalStatus;
+    private String DateBaptism;
+    private String churchBaptism;
+    private String cityBaptism;
+    private String admissionDate;
+    private Date dateConsecrationDeacon;
+    private String placeConsecrationDeacon;
+    private Date dateSeparationCooperator;
+    private String placeSeparationCooperator;
+    private Date dateConsecrationPriest;
+    private String placeConsecrationPriest;
+    private Date dateConsecrationEvangelist;
+    private String placeConsecrationEvangelist;
+    private Date dateConsecrationPastor;
+    private String placeConsecrationPastor;
+    private Date dateConsecrationMissionary;
+    private String placeConsecrationMissionary;
+    private String provenance;
+    private String recognizedThistle;
+    private String dataRecognizedThistle;
+    private String observation;
+}
