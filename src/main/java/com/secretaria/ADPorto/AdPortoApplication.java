b2f384923d0f5fea1a3e9ffdf7a2b334d98b0678
izau <izauaraujo@gmail.com>
2022-10-04 09:46:41 -0300

    Implementação Cors

--- src/main/java/com/secretaria/ADPorto/AdPortoApplication.java
+++ src/main/java/com/secretaria/ADPorto/AdPortoApplication.java
@@ -2,6 +2,10 @@ package com.secretaria.ADPorto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AdPortoApplication {
@@ -10,4 +14,9 @@ public class AdPortoApplication {
 		SpringApplication.run(AdPortoApplication.class, args);
 	}
 
+
 }
+
+
+
+
---
+++ src/main/java/com/secretaria/ADPorto/security/CorsConfiguration.java
@@ -0,0 +1,21 @@
+package com.secretaria.ADPorto.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
