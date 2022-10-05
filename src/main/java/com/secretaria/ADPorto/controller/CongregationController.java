993897a5f21aeacfff190cdc79decd6e626a2c8f
izau <izauaraujo@gmail.com>
2022-10-05 11:43:49 -0300

    Atualização 2 Implementação Cors

--- src/main/java/com/secretaria/ADPorto/controller/CongregationController.java
+++ src/main/java/com/secretaria/ADPorto/controller/CongregationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/adporto")
 
@@ -21,6 +21,7 @@ public class CongregationController {
     private CongregationService congregationService;
 
 //izau
+
      @PostMapping("/createMember")
         public String postCongregationMember(@RequestBody Member member) throws ExecutionException, InterruptedException {
         return congregationService.postCongregationMember( member);
